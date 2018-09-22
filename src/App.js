2ec26b9339e680abe20c9e4fa9bb2061baec9db4
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-09-22 13:19:18 +0200

    sredjen css...ubaceno da se coini pojavljuju...bey da padaju

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ class App extends Component {
 
   state = {
     change: true,
+    gameStarted: false,
     cId: 0,
     winY: 4,
     winR: 8,
     message: '',
     coin: { id: 0, value: 0, x: 0, y: 0 },
     y_pos: -1,
-    value:0,
+    value: 0,
     boardMatrix: [],
     xDropedArr: [],
     sirina: 7,
@@ -24,13 +25,14 @@ class App extends Component {
     scoreR: 0
   }
 
-  gameStart = () => {   // POCETAK IGRE ... 
-    let { coin, boardMatrix, sirina, visina, xDropedArr } = this.state;
+
+  componentDidMount() {   // POCETAK IGRE ... 
+    let { coin, boardMatrix, sirina, visina, xDropedArr, gameStarted } = this.state;
 
     ///////////////////MATRICA///////////////////
     boardMatrix = boardMatrix.slice();
-
     xDropedArr = xDropedArr.slice();
+
     for (let i = 0; i < visina; i++) {
 
       let y = [];
@@ -42,15 +44,16 @@ class App extends Component {
         coin = { id: countAll, value: 0, x: j, y: i };
         y.push(coin);
       }
-      boardMatrix.push(y)
-
+      boardMatrix.push(y);
     }
-    this.setState({ boardMatrix, xDropedArr });
+    gameStarted = !gameStarted;
+    this.setState({ boardMatrix, xDropedArr, gameStarted });
+
   }
 
 
   gameReset = () => {   // RESETOVANJE IGRE ... 
-    let { coin, boardMatrix, sirina, visina, xDropedArr, message, y_pos } = this.state;
+    let { coin, boardMatrix, sirina, visina, xDropedArr, message, y_pos, gameStarted } = this.state;
 
     ///////////////////MATRICA///////////////////
     boardMatrix = boardMatrix.slice();
@@ -60,6 +63,7 @@ class App extends Component {
     xDropedArr = [];
     message = '';
     y_pos = -1;
+
 
     for (let i = 0; i < visina; i++) {
 
@@ -80,14 +84,14 @@ class App extends Component {
 
   /////////////////////////////////////////////////////////////////////////////////////////////////////
   dropCoin = (i, v, x, y) => {                   //GLAVNA funkcija    ubacivanje coin-a u verticalu i ubacivanje verticale u boardV
-    let { boardMatrix, change, message, y_pos, xDropedArr, scoreR, scoreY,value } = this.state;
+    let { boardMatrix, change, message, y_pos, xDropedArr, scoreR, scoreY, value } = this.state;
     boardMatrix = boardMatrix.slice();
 
     xDropedArr = xDropedArr.slice();
     let x_pos = x;     //  trazenje kolone u kojoj je ubacen coin i ubacivanje na dno prv put pa ya jedno mesto gore svaki drugi put
-   
+
     change = !change;
-    let y_pos_top=6;
+    let y_pos_top = 6;
 
     if (xDropedArr[x_pos].value < 7) {
       y_pos = xDropedArr[x_pos].value++;  // slaganje coina u coloni...ubacivanje coina u poseban array i pracenja tog valuea
@@ -96,7 +100,7 @@ class App extends Component {
       } else {
         value = 1;
       }
-      this.setState({ xDropedArr, y_pos, change,value });
+      this.setState({ xDropedArr, y_pos, change, value });
     }
 
     let lastCoin;
@@ -175,11 +179,11 @@ class App extends Component {
 
         if (lastCoin === dij_2_downToUpLtoR && lastCoin === dij_3_downToUpLtoR && lastCoin === dij_4_downToUpLtoR) {
           if (lastCoin === 1) {
-            message = '1 ZUTI JE POBEDIO DIJ OD LEVO KA DESNO OD GORE ka DOLE';
+            message = '1A ZUTI JE POBEDIO DIJ OD LEVO KA DESNO OD GORE ka DOLE';
             scoreY++;
             this.setState({ message, scoreY });
           } else {
-            message = '1 CRVENI JE POBEDIO DIJ OD LEVO KA DESNO OD GORE ka DOLE';
+            message = '1A CRVENI JE POBEDIO DIJ OD LEVO KA DESNO OD GORE ka DOLE';
             scoreR++;
             this.setState({ message, scoreR });
           }
@@ -196,11 +200,11 @@ class App extends Component {
 
         if (lastCoin === dij_2_UptoDownLtoR && lastCoin === dij_3_UptoDownLtoR && lastCoin === dij_4_UptoDownLtoR) {
           if (lastCoin === 1) {
-            message = '2 ZUTI JE POBEDIO DIJ OD LEVO KA DESNO OD DOLE KA GORE';
+            message = '2A ZUTI JE POBEDIO DIJ OD LEVO KA DESNO OD DOLE KA GORE';
             scoreY++;
             this.setState({ message, scoreY });
           } else {
-            message = '2 CRVENI JE POBEDIO DIJ OD LEVO KA DESNO OD DOLE KA GORE';
+            message = '2A CRVENI JE POBEDIO DIJ OD LEVO KA DESNO OD DOLE KA GORE';
             scoreR++;
             this.setState({ message, scoreR });
           }
@@ -219,11 +223,11 @@ class App extends Component {
 
         if (lastCoin === dij_2_downToUpRtoL && lastCoin === dij_3_downToUpRtoL && lastCoin === dij_4_downToUpRtoL) {
           if (lastCoin === 1) {
-            message = '3 ZUTI JE POBEDIO DIJ OD DESNO KA LEVO OD  DOLE KA GORE';
+            message = '3B ZUTI JE POBEDIO DIJ OD DESNO KA LEVO OD  DOLE KA GORE';
             scoreY++;
             this.setState({ message, scoreY });
           } else {
-            message = '3 CRVENI JE POBEDIO DIJ OD DESNO KA LEVO OD  DOLE KA GORE';
+            message = '3B CRVENI JE POBEDIO DIJ OD DESNO KA LEVO OD  DOLE KA GORE';
             scoreR++;
             this.setState({ message, scoreR });
           }
@@ -232,19 +236,19 @@ class App extends Component {
     }
 
     /// od gore ka dole
-    if (x_pos < 4) {
+    if (x_pos > 2) {
       if (y_pos > 2) {
-        let dij_2_UptoDownRtoL = boardMatrix[y_pos - 1][x_pos + 1].value;
-        let dij_3_UptoDownRtoL = boardMatrix[y_pos - 2][x_pos + 2].value;
-        let dij_4_UptoDownRtoL = boardMatrix[y_pos - 3][x_pos + 3].value;
+        let dij_2_UptoDownRtoL = boardMatrix[y_pos - 1][x_pos - 1].value;
+        let dij_3_UptoDownRtoL = boardMatrix[y_pos - 2][x_pos - 2].value;
+        let dij_4_UptoDownRtoL = boardMatrix[y_pos - 3][x_pos - 3].value;
 
         if (lastCoin === dij_2_UptoDownRtoL && lastCoin === dij_3_UptoDownRtoL && lastCoin === dij_4_UptoDownRtoL) {
           if (lastCoin === 1) {
-            message = '4 ZUTI JE POBEDIO DIJ OD DESNO KA LEVO OD KA GORE DA DOLE';
+            message = '4B ZUTI JE POBEDIO DIJ OD DESNO KA LEVO OD KA GORE DA DOLE';
             scoreY++;
             this.setState({ message, scoreY });
           } else {
-            message = '4 CRVENI JE POBEDIO DIJ OD DESNO KA LEVO OD KA GORE DA DOLE';
+            message = '4B CRVENI JE POBEDIO DIJ OD DESNO KA LEVO OD KA GORE DA DOLE';
             scoreR++;
             this.setState({ message, scoreR });
           }
@@ -305,10 +309,9 @@ class App extends Component {
       <div className='container-main'>
         <ScoreBoard scoreY={scoreY} scoreR={scoreR} />
         <div className='pobeda'>
+          <button onClick={this.gameReset}>NEW GAME</button>
           - {this.state.message}
         </div>
-        <button onClick={this.gameStart}>START</button>
-        <button onClick={this.gameReset}>RESET</button>
         {tabla}
       </div>
     );
--- src/Tabla.js
+++ src/Tabla.js
@@ -4,19 +4,17 @@ import React, { Component } from 'react';
 class Tabla extends Component {
 
     render() {
-        let classN='coin-neutral';
+        let classN='';
         if(this.props.v===1){
             classN='coin-yellow';
         }else if(this.props.v===2){
             classN='coin-red';
         }
 
-        return ( 
-             
-                 
-                    <div className={classN} onClick={this.props.clicked}>
-                   
-                      x{this.props.x} y{this.props.y}
+        return (      
+                    <div className='coin-neutral'  onClick={this.props.clicked}>
+                     <div className={classN}> x{this.props.x} y{this.props.y}</div>
+                     
                     </div>   
              
         );
